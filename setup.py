a7072891ca7a9d29310f6e77b8b2eb526065d0e0
sbissoyi <swarupananda@gmail.com>
2024-12-15 03:12:06 +0530

    Project homepage path corrected

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="shabdamegha",
-    version="0.1.6",
+    version="0.1.7",
     author="Dr. Swarupananda Bissoyi",
     author_email="swarupananda@gmail.com",
     description="A python library to generate word cloud for Odia language with proper rendering of Ligatures (Yuktakshars). ଯୁକ୍ତାକ୍ଷର ଗୁଡ଼ିକୁ ସଠିକ୍‌ ପ୍ରଦର୍ଶିତ କରି ଶବ୍ଦମେଘ ସୃଷ୍ଟି କରିବା ନିମନ୍ତେ ଏକ ପାଇଥନ୍‌ ଲାଇବ୍ରେରୀ",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/sbissoyi/shabdamegha/docs",
+    url="https://sbissoyi.github.io/shabdamegha/docs",
     project_urls={
         "Documentation": "https://sbissoyi.github.io/shabdamegha/docs",
         "Source": "https://github.com/sbissoyi/shabdamegha/"
